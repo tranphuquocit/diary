c0db9c0c2fca6eaaeac1858e209113cd81117fde
Zin <quoctp@icheck.vn>
2021-03-15 09:05:08 +0700

    old commit

--- src/app/layouts/sam/sam.component.ts
+++ src/app/layouts/sam/sam.component.ts
@@ -8,38 +8,70 @@ import { Component } from '@angular/core';
 export class SamComponent {
   player: any = [
     {
+      name: '',
       tong: 0,
+      winner: null,
       diem: []
     },
     {
+      name: '',
       tong: 0,
+      winner: null,
       diem: []
     },
     {
+      name: '',
       tong: 0,
+      winner: null,
       diem: []
     },
     {
+      name: '',
       tong: 0,
+      winner: null,
       diem: []
     },
     {
+      name: '',
       tong: 0,
+      winner: null,
       diem: []
     }
   ];
-  times: any = [];
-  constructor() {
-    this.times = [1]
-  }
+  times: any = [1];
   vongChoiCu = 0;
   diemVongChoiCu = [];
+  constructor() {
+    let sam = localStorage.getItem('sam');
+    if(sam) {
+      this.player = JSON.parse(sam).player;
+      this.times = JSON.parse(sam).times;
+      this.vongChoiCu = JSON.parse(sam).vongChoiCu;
+    }
+  }
+  inputName(name: string, id) {
+    this.player[id].name = name;
+  }
+  keyCode: number = 0;
+  inputCode(code) {
+    this.keyCode = code
+  }
   sumPoint(event: any, vong_choi, nguoi_choi) {
+    // console.log(this.keyCode)
     this.vongChoiCu = vong_choi;
     this.vongChoiCu > vong_choi ? this.diemVongChoiCu = [] : this.diemVongChoiCu;
     let input = +event.target.value
+    // console.log(input)
     if (input || input === 0) {
-      this.player[nguoi_choi].diem[vong_choi] = (input > 0 ? -input : '-');
+      if(this.keyCode === 189) {
+        this.player[nguoi_choi].diem[vong_choi] = '-';
+      } else if(this.keyCode === 69) {
+        this.player[nguoi_choi].diem[vong_choi] = 'e';        
+      } else if(this.keyCode === 48 && input === 0) {
+        this.player[nguoi_choi].diem[vong_choi] = 0;        
+      } else {
+        this.player[nguoi_choi].diem[vong_choi] = (input > 0 ? -input : '-');
+      }
       this.diemVongChoiCu = []
       this.player.forEach((ngChoi) => {
         if (ngChoi.diem[vong_choi]) {
@@ -48,32 +80,36 @@ export class SamComponent {
       });
     }
   }
-  tongDiemNguoiChoi(vong_choi, nguoi_choi) {
-    this.player.forEach((pl: any, index) => {
-      // Tông điểm của 1 người chơi
-      if (pl.diem.length > 0) {
-        pl.tong = pl.diem.reduce(this.sum);
-      }
-    })
-  }
   vanTiep() {
     if(this.vongChoiCu > 0) {
       this.times.push(this.times[this.times.length - 1] + 1);
       this.player.forEach(pl => {
-        if(pl.diem[this.vongChoiCu] === '-') {
+        if(pl.diem[this.vongChoiCu] === '-') { 
           pl.tong = pl.tong - this.diemVongChoiCu.reduce(this.sum);
-        } else if(pl.diem[this.vongChoiCu] === 0) {
-          pl.tong = pl.tong - 20
         } else {
-          pl.tong = pl.tong + pl.diem[this.vongChoiCu];
+          if(pl.diem[this.vongChoiCu] !== '-' && pl.diem[this.vongChoiCu] !== 'e') {
+            pl.tong = pl.tong + pl.diem[this.vongChoiCu];
+          }
         }
       })
     }
+    let sam = {
+      player: this.player,
+      times: this.times,
+      vongChoiCu: this.vongChoiCu
+    }
+    localStorage.setItem('sam', JSON.stringify(sam));
   }
-  chatHeo: boolean = false;
-  checkKey(key) {
-    console.log(key)
-    // dấu - là 189
+  showThanhToan() {
+    alert(`
+    ${this.player[0].name} ${this.player[0].tong < 0 ? 'Số tiền thanh toán là ': 'Chúc mừng bạn nhận được '} ${this.player[0].tong}
+    ${this.player[1].name} ${this.player[1].tong < 0 ? 'Số tiền thanh toán là ': 'Chúc mừng bạn nhận được '} ${this.player[1].tong}
+    ${this.player[2].name} ${this.player[2].tong < 0 ? 'Số tiền thanh toán là ': 'Chúc mừng bạn nhận được '} ${this.player[2].tong}
+    ${this.player[3].name} ${this.player[3].tong < 0 ? 'Số tiền thanh toán là ': 'Chúc mừng bạn nhận được '} ${this.player[3].tong}
+    ${this.player[4].name} ${this.player[4].tong < 0 ? 'Số tiền thanh toán là ': 'Chúc mừng bạn nhận được '} ${this.player[4].tong}
+    `);
+    localStorage.removeItem('sam');
+    window.location.reload();
   }
   sum(a, b) {
     return a + b;
--- src/app/layouts/sam/sam.module.ts
+++ src/app/layouts/sam/sam.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SamRoutingModule } from './sam.routing';
 import { SamComponent } from './sam.component';
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     SamComponent
   ],
   imports: [
     CommonModule,
+    FormsModule,
     SamRoutingModule
   ],
   providers: [],
