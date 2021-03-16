ebe8e0efb845bf5c6895b2753bdcedbc5209a120
Zin <quoctp@icheck.vn>
2021-03-16 09:44:11 +0700

    Fix

--- src/app/layouts/sam/sam.component.ts
+++ src/app/layouts/sam/sam.component.ts
@@ -34,6 +34,7 @@ export class SamComponent {
     }
   ];
   times: any = [1];
+  keyCode: number = 0;
   constructor() {
     let sam = {
       player: this.player,
@@ -49,24 +50,74 @@ export class SamComponent {
       localStorage.setItem('sam',JSON.stringify(sam));
     }
   }
+  inputCode(code) {
+    this.keyCode = code
+  }
   vongChoiCu = 0;
   diemVongChoiCu = [];
   inputName(name: string, index: number) {
     this.player[index].name = name;
   }
   sumPoint(event: any, vong_choi, nguoi_choi) {
+    this.vongChoiCu = vong_choi;
+    this.vongChoiCu > vong_choi ? this.diemVongChoiCu = [] : this.diemVongChoiCu;
+    let input = +event.target.value
+    if (input || input === 0) {
+      if(this.keyCode === 189) {
+        this.player[nguoi_choi].diem[vong_choi] = '-';
+      } else if(this.keyCode === 69) {
+        this.player[nguoi_choi].diem[vong_choi] = 'e';        
+      } else if(this.keyCode === 48 && input === 0) {
+        this.player[nguoi_choi].diem[vong_choi] = 0;        
+      } else {
+        this.player[nguoi_choi].diem[vong_choi] = (input > 0 ? -input : '-');
+      }
+      this.diemVongChoiCu = []
+      this.player.forEach((ngChoi) => {
+        if (ngChoi.diem[vong_choi]) {
+          this.diemVongChoiCu.push(ngChoi.diem[vong_choi] === '-' ? 0 : ngChoi.diem[vong_choi])
+        }
+      });
+    }
   }
   checkEvent(event: any, vong_choi: number, nguoi_choi: number) {
-    console.log(event.target.value);
-    this.player[nguoi_choi].diem[vong_choi] = +event.target.value;
+    // if(event.target.value)
+    let input = event.target.value
+    if(input === 0) {
+      this.player[nguoi_choi].diem[vong_choi] = 0;
+    } else if (input === '-') {
+      this.player[nguoi_choi].diem[vong_choi] = '-';      
+    } else {
+      this.player[nguoi_choi].diem[vong_choi] = (input > 0 ? -input : 0);
+    }
+    this.diemVongChoiCu = []
+    this.player.forEach((ngChoi) => {
+      if (ngChoi.diem[vong_choi]) {
+        this.diemVongChoiCu.push(ngChoi.diem[vong_choi] === '-' ? 0 : ngChoi.diem[vong_choi])
+      }
+    });
   }
   vanTiep() {
     console.log(this.player)
-  }
-  chatHeo: boolean = false;
-  checkKey(key) {
-    // console.log(key)
-    // dấu - là 189
+    if(this.vongChoiCu > 0) {
+      this.times.unshift(this.times[0] + 1);
+      // this.times.push(this.times[this.times.length - 1] + 1);
+      this.player.forEach(pl => {
+        if(pl.diem[this.vongChoiCu] === '-') { 
+          pl.tong = pl.tong - this.diemVongChoiCu.reduce(this.sum);
+        } else {
+          if(pl.diem[this.vongChoiCu] !== '-' && pl.diem[this.vongChoiCu] !== 'e') {
+            pl.tong = pl.tong + pl.diem[this.vongChoiCu];
+          }
+        }
+      })
+    }
+    let sam = {
+      player: this.player,
+      times: this.times,
+      vongChoiCu: this.vongChoiCu
+    }
+    localStorage.setItem('sam', JSON.stringify(sam));
   }
   sum(a, b) {
     return a + b;
